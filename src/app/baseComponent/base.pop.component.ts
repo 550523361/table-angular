7c613eb8f51a1de56aeef9165f962adb0ffaea5d
550523361 <550523361>
2017-05-15 23:45:15 +0800

    修改弹窗测试

--- src/app/baseComponent/base.pop.component.ts
+++ src/app/baseComponent/base.pop.component.ts
@@ -15,80 +15,16 @@ import {TableListSimpleConfig} from "../model/table.list.simple.config.model";
 })
 export class BasePopComponent implements OnInit,OnChanges,DoCheck{
 
-    /*@Input()*/
+    @Input()
     tableListConfig:TableListConfig;
 
     @Input()
     set simpleConfig(data:TableListSimpleConfig){
-        this._keyValueDiffer = this._keyValueDiffers.find(data).create(null);
-        this.simpleConfigData=data;
+        /*this._keyValueDiffer = this._keyValueDiffers.find(data).create(null);
+        this.simpleConfigData=data;*/
     };
     private _keyValueDiffer: KeyValueDiffer;
     private simpleConfigData:TableListSimpleConfig;
-    query={
-        queryElements:[
-            /*{
-                label:'请选择省',
-                type:'select',
-                placeholder:'请输入省',
-                dataUrl:'citys/queryOpeningCitiesList.json',
-                prop:'provinceId',
-                initQuery:true,
-                defaultValue:"",
-                propList:'provinceList',
-                propKeyList:'provinceList',
-                propValueList:'citiesList',
-                casecadeParen:"",
-                casecadeChild:"cityId",
-                casecadeGrandsonList:["cityId","districtId","communityId"]
-            },
-            {
-                label:'请选择市',
-                type:'select',
-                defaultValue:"",
-                placeholder:'请输入市',
-                dataUrl:'citys/queryOpeningCitiesList.json',
-                prop:'cityId',
-                extendsProp:["pid"],
-                propList:'cityList',
-                propKeyList:'cityList',
-                propValueList:'citiesList',
-                casecadeParen:"provinceId",
-                casecadeChild:"districtId",
-                casecadeGrandsonList:["districtId","communityId"]
-            },
-            {
-                label:'请选择区',
-                defaultValue:"",
-                type:'select',
-                dataUrl:'citys/queryOpeningCitiesList.json',
-                placeholder:'请选择区',
-                prop:'districtId',
-                extendsProp:["pid"],
-                propList:'districtList',
-                propKeyList:'districtList',
-                propValueList:'citiesList',
-                casecadeParen:"cityId",
-                casecadeChildDataUrl:"community/queryCommunitiesList.json",
-                casecadeChildListProp:"data",
-                casecadeChild:"",
-                queryParam:"cityId"
-            },*/
-            {
-                label:'小区名称',
-                type:'input',
-                placeholder:'小区名称',
-                prop:'communityIdStr'
-            },
-            {
-                label:'商品名称',
-                type:'input',
-                placeholder:'商品名称',
-                prop:'name'
-            }
-        ]
-    };
-
 
     constructor(
         public _iterableDiffers: IterableDiffers, public _keyValueDiffers: KeyValueDiffers
@@ -97,7 +33,7 @@ export class BasePopComponent implements OnInit,OnChanges,DoCheck{
     }
 
     ngOnChanges(changes: {[propKey: string]: SimpleChange}){
-        //console.log("this.simpleConfig",this.simpleConfig)
+        console.log("this.simpleConfig ngOnChanges")
         for (let propName in changes) {
             let changedProp = changes[propName];
             console.log(propName,changedProp);
@@ -106,13 +42,13 @@ export class BasePopComponent implements OnInit,OnChanges,DoCheck{
     }
 
     ngDoCheck():void{
-        if(this._keyValueDiffers) {
+        /*if(this._keyValueDiffers) {
             const changes = this._keyValueDiffer.diff(this.simpleConfigData);
             if (changes) {
                 console.log("this.simpleConfig",changes)
                 this.whatIsChanged(changes);
             }
-        }
+        }*/
     }
 
     whatIsChanged(changes){
@@ -133,23 +69,6 @@ export class BasePopComponent implements OnInit,OnChanges,DoCheck{
         });
     }
 
-    createTableListConfig(data){
-
-    }
-
     ngOnInit(){
-        let tableListConfig=new TableListConfig("advert/queryAdvertList.json",
-            null,
-            [{label:"选择",click:function (item) {console.log(item)}}],
-            this.query.queryElements,"post"
-        );
-        tableListConfig.defaultColumsHeaderMap={
-            name:"名称",
-            communityName:"小区名称",
-            startDate:"开始时间",
-            endDate:"结束时间",
-            pageTypeName:"页面类型"
-        };
-        this.tableListConfig=tableListConfig;
     }
 }
--- src/app/test/base.test.pop.component.ts
+++ src/app/test/base.test.pop.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {TableListSimpleConfig} from "../model/table.list.simple.config.model";
+import {TableListConfig} from "../model/table.list.config.model";
 /**
  * Created by Administrator on 2017/5/15.
  */
@@ -15,47 +16,131 @@ declare var _;
     styleUrls:["base.test.pop.component.css"]
 })
 export  class BaseTestPopComponent implements OnInit{
-    simpleConfig:TableListSimpleConfig;
+    tableListConfig:TableListConfig;
+    query={
+        queryElements:[
+      /*{
+       label:'请选择省',
+       type:'select',
+       placeholder:'请输入省',
+       dataUrl:'citys/queryOpeningCitiesList.json',
+       prop:'provinceId',
+       initQuery:true,
+       defaultValue:"",
+       propList:'provinceList',
+       propKeyList:'provinceList',
+       propValueList:'citiesList',
+       casecadeParen:"",
+       casecadeChild:"cityId",
+       casecadeGrandsonList:["cityId","districtId","communityId"]
+       },
+       {
+       label:'请选择市',
+       type:'select',
+       defaultValue:"",
+       placeholder:'请输入市',
+       dataUrl:'citys/queryOpeningCitiesList.json',
+       prop:'cityId',
+       extendsProp:["pid"],
+       propList:'cityList',
+       propKeyList:'cityList',
+       propValueList:'citiesList',
+       casecadeParen:"provinceId",
+       casecadeChild:"districtId",
+       casecadeGrandsonList:["districtId","communityId"]
+       },
+       {
+       label:'请选择区',
+       defaultValue:"",
+       type:'select',
+       dataUrl:'citys/queryOpeningCitiesList.json',
+       placeholder:'请选择区',
+       prop:'districtId',
+       extendsProp:["pid"],
+       propList:'districtList',
+       propKeyList:'districtList',
+       propValueList:'citiesList',
+       casecadeParen:"cityId",
+       casecadeChildDataUrl:"community/queryCommunitiesList.json",
+       casecadeChildListProp:"data",
+       casecadeChild:"",
+       queryParam:"cityId"
+       },*/
+      {
+        label:'小区名称',
+        type:'input',
+        placeholder:'小区名称',
+        prop:'communityIdStr'
+      },
+      {
+        label:'商品名称',
+        type:'input',
+        placeholder:'商品名称',
+        prop:'name'
+      },
+      {
+        label:'搜索',
+        type:'search'
+      }
+    ]
+  };
     ngOnInit(){
-        this.simpleConfig=new TableListSimpleConfig("acs");
-       /* setInterval(data=>{
-            let name=Math.ceil(Math.random()*10000);
-            this.simpleConfig.watcher={name:name};
-            console.log("nameChanged",name)
-        },5000)*/
+      let tableListConfig=new TableListConfig("advert/queryAdvertList.json",
+        null,
+        [{label:"选择",click:data=>{
+          this.chooseSimple(data);
+        }}],
+        this.query.queryElements,"post"
+      );
+      tableListConfig.defaultColumsHeaderMap={
+        name:"名称",
+        communityName:"小区名称",
+        startDate:"开始时间",
+        endDate:"结束时间",
+        pageTypeName:"页面类型"
+      };
+      this.tableListConfig=tableListConfig;
     }
+    advert;
+    chooseSimple(data){
+      this.advert=data;
+      this.tableListConfig.operator[0].label="已选"
+      layer.close(this.popId);
+    }
+
+    popId;
+
     popTest(){
-        this.simpleConfig.watcher={name:"abc"+Math.ceil(Math.random()*10000)};
         this.popNoCopy($("#testPop"),"70%","60%");
     }
 
     popNoCopy(dom, width?, height?, callBack?, data?) {
-    if (width == undefined) {
-        width = "80%";
-    }
-    if (height == undefined) {
-        height = "80%";
-    }
-    if (!_.contains(width + "", "%")) {
-        width += "px";
-    }
-    if (!_.contains(height + "", "%")) {
-        height += "px";
-    }
-    layer.open({
-        title: null,
-        type: 1,
-        content: dom,
-        btn: null,
-        closeBtn: null,
-        zIndex: layer.zIndex,
-        area: [width, height],
-        success: function (layer, index) {
-            if (typeof  callBack == 'function') {
-                callBack(data);
-            }
+        if (width == undefined) {
+            width = "80%";
         }
-    })
-}
+        if (height == undefined) {
+            height = "80%";
+        }
+        if (!_.contains(width + "", "%")) {
+            width += "px";
+        }
+        if (!_.contains(height + "", "%")) {
+            height += "px";
+        }
+        this.popId=layer.open({
+            title: null,
+            type: 1,
+            content: dom,
+            btn: null,
+            closeBtn: null,
+            zIndex: layer.zIndex,
+            area: [width, height],
+            success: function (layer, index) {
+                if (typeof  callBack == 'function') {
+                    callBack(data);
+                }
+            }
+        })
+  }
 
 }
